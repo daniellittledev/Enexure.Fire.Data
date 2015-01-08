0b8dc0dc4e346303e3046aaf965475685f3283be
Daniel Little <lavinski@live.com.au>
2015-01-08 21:26:42 +1000

    Get version number from appveyor

--- build/Runner.ps1
+++ build/Runner.ps1
@@ -1,4 +1,6 @@
-param ($target, $version)
+param ($target)
+
+$version = $Env:APPVEYOR_BUILD_VERSION
 
 Write-Host "PSake target: $target"
 Write-Host "Build version: $version"
@@ -7,4 +9,4 @@ $ErrorActionPreference = "Stop"
 
 Import-Module "$PSScriptRoot\modules\psake\psake.psm1"
 
-Invoke-Psake "$PSScriptRoot\Build.ps1" $target $version
+Invoke-Psake "$PSScriptRoot\Build.ps1" -Parameters @{ "target" = $target; "version" = $version }  
